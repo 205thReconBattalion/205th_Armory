6d7c79b78b3d779d90f92dc6fd970f75824bae7f
Spark2000 <paulalex.wetzel@gmail.com>
2023-11-13 14:25:29 +0100

    add: config.cpp
     (leere Version)

---
+++ addons/RB205_main/config.cpp
@@ -0,0 +1,30 @@
+class cfgPatches
+{
+    class RB205_main
+	{
+		requiredAddons[]=
+		{
+			
+		};
+		requiredVersion = 1.0;
+        author = "205th Recon Battalion"
+		weapons[] =
+		{
+			
+		};
+		units[] =
+		{
+			
+		};
+	};
+};
+
+class cfgWeapons
+{
+    
+};
+
+class cfgVehicles
+{
+	
+};
