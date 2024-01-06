6a67eca476af102c35c021d97d7b55bfa6e81a65
Spark2000 <paulalex.wetzel@gmail.com>
2024-01-06 15:19:57 +0100

    add: LAAT

---
+++ addons/RB205_vehicles/laat/config.cpp
@@ -0,0 +1,317 @@
+class cfgPatches
+{
+    class RB205_vehicles_laat
+	{
+		requiredAddons[]=
+		{
+			"RB205_vehicles",
+			"3AS_LAAT"
+		};
+		requiredVersion = 1.0;
+        author = "205th Recon Battalion";
+		weapons[] =	{};
+		units[] =
+		{
+			"RB205_laat",
+			"RB205_laat_lights",
+			"RB205_laat_turrets"
+		};
+	};
+};
+
+#include "\RB205_vehicles\macros.hpp"
+#include "\RB205_vehicles\inventory.hpp"
+
+class cfgWeapons {};
+class cfgVehicles
+{
+	class 3as_LAAT_Mk1;
+	class 3as_LAAT_Mk1Lights;
+	class 3as_LAAT_Mk2;
+
+	class RB205_laat: 3as_LAAT_Mk2
+	{
+		ACCESS_TRUE
+		displayName = "LAAT/I";
+		author = "205th Recon Battalion";
+		crew = "RB205_clone_plt_trooper";
+		VEH_INVENTORY
+		CARGO_L
+		//Editor/ Zeus
+		side = 1;
+		faction = "RB205";
+		editorSubcategory = "RB205_veh_air";
+		editorPreview = "";
+		//Textures
+		hiddenSelectionsTextures[] =
+		{
+			"RB205_vehicles\laat\data\laat_hull_co.paa",
+			"RB205_vehicles\laat\data\laat_wings_co.paa",
+			"RB205_vehicles\laat\data\laat_weapons_co.paa",
+			"RB205_vehicles\laat\data\laat_weapon_details_co.paa",
+			"RB205_vehicles\laat\data\laat_interior_co.paa"
+		};
+		class TextureSources
+		{
+			class Default
+			{
+				displayName = "205th Recon Battalion";
+				author = "205th Recon Battalion";
+				textures[] =
+				{
+					"RB205_vehicles\laat\data\laat_hull_co.paa",
+					"RB205_vehicles\laat\data\laat_wings_co.paa",
+					"RB205_vehicles\laat\data\laat_weapons_co.paa",
+					"RB205_vehicles\laat\data\laat_weapon_details_co.paa",
+					"RB205_vehicles\laat\data\laat_interior_co.paa"
+				};
+				factions[] =
+				{
+					"RB205"
+				};
+			};
+			class Republic
+			{
+				displayName = "Grand Army of the Republic";
+				author = "3AS";
+				textures[] =
+				{
+					"3AS\3as_Laat\LAATI\data\Hull_CO.paa",
+					"3AS\3as_Laat\LAATI\data\wings_CO.paa",
+					"3AS\3as_Laat\LAATI\data\weapons_CO.paa",
+					"3AS\3as_Laat\LAATI\data\weapon_Details_CO.paa",
+					"3AS\3as_Laat\LAATI\data\interior_CO.paa"
+				};
+			};
+			class Republic_91
+			{
+				displayName = "Grand Army of the Republic - 91st";
+				author = "3AS";
+				textures[] =
+				{
+					"3AS\3as_Laat\LAATI\data\Hull_91_CO.paa",
+					"3AS\3as_Laat\LAATI\data\wings_CO.paa",
+					"3AS\3as_Laat\LAATI\data\weapons_CO.paa",
+					"3AS\3as_Laat\LAATI\data\weapon_Details_CO.paa",
+					"3AS\3as_Laat\LAATI\data\interior_CO.paa"
+				};
+			};
+			class Republic_ARC
+			{
+				displayName = "Grand Army of the Republic - ARC";
+				author = "3AS";
+				textures[] =
+				{
+					"3AS\3as_Laat\LAATI\data\hull_arc_co.paa",
+					"3AS\3as_Laat\LAATI\data\wings_arc_co.paa",
+					"3AS\3as_Laat\LAATI\data\weapons_arc_co.paa",
+					"3AS\3as_Laat\LAATI\data\weapon_details_co.paa",
+					"3AS\3as_Laat\LAATI\data\interior_co.paa"
+				};
+			};
+			class Empire
+			{
+				displayName = "Galactic Empire";
+				author = "3AS";
+				textures[] =
+				{
+					"3AS\3as_Laat\LAATI\data\Hull_Imp_CO.paa",
+					"3AS\3as_Laat\LAATI\data\wings_Imp_CO.paa",
+					"3AS\3as_Laat\LAATI\data\weapons_Imp_CO.paa",
+					"3AS\3as_Laat\LAATI\data\weapon_Details_Imp_CO.paa",
+					"3AS\3as_Laat\LAATI\data\interior_Imp_CO.paa"
+				};
+			};
+		};
+	};
+	class RB205_laat_lights: 3as_LAAT_Mk1Lights
+	{
+		ACCESS_TRUE
+		displayName = "LAAT/I (Lights)";
+		author = "205th Recon Battalion";
+		crew = "RB205_clone_plt_trooper";
+		VEH_INVENTORY
+		CARGO_L
+		//Editor/ Zeus
+		side = 1;
+		faction = "RB205";
+		editorSubcategory = "RB205_veh_air";
+		editorPreview = "";
+		//Textures
+		hiddenSelectionsTextures[] =
+		{
+			"RB205_vehicles\laat\data\laat_hull_co.paa",
+			"RB205_vehicles\laat\data\laat_wings_co.paa",
+			"RB205_vehicles\laat\data\laat_weapons_co.paa",
+			"RB205_vehicles\laat\data\laat_weapon_details_co.paa",
+			"RB205_vehicles\laat\data\laat_interior_co.paa"
+		};
+		class TextureSources
+		{
+			class Default
+			{
+				displayName = "205th Recon Battalion";
+				author = "205th Recon Battalion";
+				textures[] =
+				{
+					"RB205_vehicles\laat\data\laat_hull_co.paa",
+					"RB205_vehicles\laat\data\laat_wings_co.paa",
+					"RB205_vehicles\laat\data\laat_weapons_co.paa",
+					"RB205_vehicles\laat\data\laat_weapon_details_co.paa",
+					"RB205_vehicles\laat\data\laat_interior_co.paa"
+				};
+				factions[] =
+				{
+					"RB205"
+				};
+			};
+			class Republic
+			{
+				displayName = "Grand Army of the Republic";
+				author = "3AS";
+				textures[] =
+				{
+					"3AS\3as_Laat\LAATI\data\Hull_CO.paa",
+					"3AS\3as_Laat\LAATI\data\wings_CO.paa",
+					"3AS\3as_Laat\LAATI\data\weapons_CO.paa",
+					"3AS\3as_Laat\LAATI\data\weapon_Details_CO.paa",
+					"3AS\3as_Laat\LAATI\data\interior_CO.paa"
+				};
+			};
+			class Republic_91
+			{
+				displayName = "Grand Army of the Republic - 91st";
+				author = "3AS";
+				textures[] =
+				{
+					"3AS\3as_Laat\LAATI\data\Hull_91_CO.paa",
+					"3AS\3as_Laat\LAATI\data\wings_CO.paa",
+					"3AS\3as_Laat\LAATI\data\weapons_CO.paa",
+					"3AS\3as_Laat\LAATI\data\weapon_Details_CO.paa",
+					"3AS\3as_Laat\LAATI\data\interior_CO.paa"
+				};
+			};
+			class ARC
+			{
+				displayName = "Grand Army of the Republic - ARC";
+				author = "3AS";
+				textures[] =
+				{
+					"3AS\3as_Laat\LAATI\data\hull_arc_co.paa",
+					"3AS\3as_Laat\LAATI\data\wings_arc_co.paa",
+					"3AS\3as_Laat\LAATI\data\weapons_arc_co.paa",
+					"3AS\3as_Laat\LAATI\data\weapon_details_co.paa",
+					"3AS\3as_Laat\LAATI\data\interior_co.paa"
+				};
+			};
+			class Empire
+			{
+				displayName = "Galactic Empire";
+				author = "3AS";
+				textures[] =
+				{
+					"3AS\3as_Laat\LAATI\data\Hull_Imp_CO.paa",
+					"3AS\3as_Laat\LAATI\data\wings_Imp_CO.paa",
+					"3AS\3as_Laat\LAATI\data\weapons_Imp_CO.paa",
+					"3AS\3as_Laat\LAATI\data\weapon_Details_Imp_CO.paa",
+					"3AS\3as_Laat\LAATI\data\interior_Imp_CO.paa"
+				};
+			};
+		};
+	};
+	class RB205_laat_turrets: 3as_LAAT_Mk1
+	{
+		ACCESS_TRUE
+		displayName = "LAAT/I (Turrets)";
+		author = "205th Recon Battalion";
+		crew = "RB205_clone_plt_trooper";
+		VEH_INVENTORY
+		CARGO_L
+		//Editor/ Zeus
+		side = 1;
+		faction = "RB205";
+		editorSubcategory = "RB205_veh_air";
+		editorPreview = "";
+		//Textures
+		hiddenSelectionsTextures[] =
+		{
+			"RB205_vehicles\laat\data\laat_hull_co.paa",
+			"RB205_vehicles\laat\data\laat_wings_co.paa",
+			"RB205_vehicles\laat\data\laat_weapons_co.paa",
+			"RB205_vehicles\laat\data\laat_weapon_details_co.paa",
+			"RB205_vehicles\laat\data\laat_interior_co.paa"
+		};
+		class TextureSources
+		{
+			class Default
+			{
+				displayName = "205th Recon Battalion";
+				author = "205th Recon Battalion";
+				textures[] =
+				{
+					"RB205_vehicles\laat\data\laat_hull_co.paa",
+					"RB205_vehicles\laat\data\laat_wings_co.paa",
+					"RB205_vehicles\laat\data\laat_weapons_co.paa",
+					"RB205_vehicles\laat\data\laat_weapon_details_co.paa",
+					"RB205_vehicles\laat\data\laat_interior_co.paa"
+				};
+				factions[] =
+				{
+					"RB205"
+				};
+			};
+			class Republic
+			{
+				displayName = "Grand Army of the Republic";
+				author = "3AS";
+				textures[] =
+				{
+					"3AS\3as_Laat\LAATI\data\Hull_CO.paa",
+					"3AS\3as_Laat\LAATI\data\wings_CO.paa",
+					"3AS\3as_Laat\LAATI\data\weapons_CO.paa",
+					"3AS\3as_Laat\LAATI\data\weapon_Details_CO.paa",
+					"3AS\3as_Laat\LAATI\data\interior_CO.paa"
+				};
+			};
+			class Republic_91
+			{
+				displayName = "Grand Army of the Republic - 91st";
+				author = "3AS";
+				textures[] =
+				{
+					"3AS\3as_Laat\LAATI\data\Hull_91_CO.paa",
+					"3AS\3as_Laat\LAATI\data\wings_CO.paa",
+					"3AS\3as_Laat\LAATI\data\weapons_CO.paa",
+					"3AS\3as_Laat\LAATI\data\weapon_Details_CO.paa",
+					"3AS\3as_Laat\LAATI\data\interior_CO.paa"
+				};
+			};
+			class ARC
+			{
+				displayName = "Grand Army of the Republic - ARC";
+				author = "3AS";
+				textures[] =
+				{
+					"3AS\3as_Laat\LAATI\data\hull_arc_co.paa",
+					"3AS\3as_Laat\LAATI\data\wings_arc_co.paa",
+					"3AS\3as_Laat\LAATI\data\weapons_arc_co.paa",
+					"3AS\3as_Laat\LAATI\data\weapon_details_co.paa",
+					"3AS\3as_Laat\LAATI\data\interior_co.paa"
+				};
+			};
+			class Empire
+			{
+				displayName = "Galactic Empire";
+				author = "3AS";
+				textures[] =
+				{
+					"3AS\3as_Laat\LAATI\data\Hull_Imp_CO.paa",
+					"3AS\3as_Laat\LAATI\data\wings_Imp_CO.paa",
+					"3AS\3as_Laat\LAATI\data\weapons_Imp_CO.paa",
+					"3AS\3as_Laat\LAATI\data\weapon_Details_Imp_CO.paa",
+					"3AS\3as_Laat\LAATI\data\interior_Imp_CO.paa"
+				};
+			};
+		};
+	};
+};
