a212c352dc3193d6c8f3aecac753b1fd05d3c1aa
Spark2000 <paulalex.wetzel@gmail.com>
2024-01-06 15:18:20 +0100

    add: AT-AP

---
+++ addons/RB205_vehicles/atap/config.cpp
@@ -0,0 +1,41 @@
+class cfgPatches
+{
+    class RB205_vehicles_atap
+	{
+		requiredAddons[]=
+		{
+			"RB205_vehicles",
+			"3AS_ATAP"
+		};
+		requiredVersion = 1.0;
+        author = "205th Recon Battalion";
+		weapons[] =	{};
+		units[] =
+		{
+			"RB205_atap"
+		};
+	};
+};
+
+#include "\RB205_vehicles\macros.hpp"
+#include "\RB205_vehicles\inventory.hpp"
+
+class cfgWeapons {};
+class cfgVehicles
+{
+	class 3as_ATAP_base;
+	class RB205_atap: 3as_ATAP_base
+	{
+		ACCESS_TRUE
+		displayName = "AT-AP";
+		author = "205th Recon Battalion";
+		crew = "RB205_clone_trooper";
+		VEH_INVENTORY
+		CARGO_S
+		//Editor/ Zeus
+		side = 1;
+		faction = "RB205";
+		editorSubcategory = "RB205_veh_tank";
+		editorPreview = "";
+	};
+};
