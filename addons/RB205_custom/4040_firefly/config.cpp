bbf7152bebf5d901b360db35a037e1ce48b38eb2
Oke Johannsen <oke.jo26@gmail.com>
2024-01-14 19:00:19 +0100

    fix

--- addons/RB205_custom/4040_firefly/config.cpp
+++ addons/RB205_custom/4040_firefly/config.cpp
@@ -31,7 +31,7 @@ class cfgWeapons
         hiddenSelectionsTextures[] =
             {
                 "RB205_custom\4040_firefly\data\H_firefly.paa",
-                "RB205_main\data\default\visor_p2_co.paa"
+                "RB205_main\data\airborne\visor_ab_co.paa"
             };
     };
 
