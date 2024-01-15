c290c73762d40b413a99f9755fd49f9317381be4
One <tim.one.gmc@gmail.com>
2024-01-15 23:23:57 +0100

    add: custom Slow

---
+++ addons/RB205_custom/3340_slow/config.cpp
@@ -0,0 +1,78 @@
+class cfgPatches
+{
+    class RB205_custom_slow
+    {
+        requiredAddons[] =
+        {
+            "RB205_main",
+            "RB205_custom"
+        };
+        requiredVersion = 1.0;
+        weapons[] =
+        {
+            "RB205_H_slow",
+            "RB205_V_slow",
+            "RB205_U_slow"
+        };
+        units[]	=
+        {
+            "RB205_clone_slow"
+        };
+    };
+};
+
+#include "\RB205_main\macros.hpp"
+
+class cfgWeapons
+{
+    class RB205_H_sergeant;
+    class RB205_H_slow: RB205_H_sergeant
+    {
+        displayName = "[205] Clone Trooper Helmet [3340]";
+        hiddenSelectionsTextures[] =
+        {
+            "RB205_custom\3340_slow\data\H_slow.paa",
+            "RB205_main\data\default\visor_p2_co.paa"
+        };
+    };
+
+    class RB205_V_cs;
+    class RB205_V_slow: RB205_V_cs
+    {
+        displayName = "[205] Clone Trooper Vest [3340]";
+        hiddenSelectionsTextures[] =
+        {
+            "RB205_custom\3340_slow\data\V_slow.paa"
+        };
+    };
+
+    class RB205_U_sergeant;
+    class ItemInfo;
+    class RB205_U_slow: RB205_U_sergeant
+    {
+        displayName = "[205] Clone Trooper Armor [3340]";
+        class ItemInfo: ItemInfo
+        {
+            uniformClass = RB205_clone_slow;
+        };
+    };
+};
+
+class cfgVehicles
+{
+    class RB205_clone_sergeant;
+    class RB205_clone_slow: RB205_clone_sergeant
+    {
+        displayName = "CS-3340 Slow";
+        uniformclass = "RB205_U_slow";
+        editorSubCategory = "RB205_lore";
+        hiddenselectionsTextures[] =
+        {
+            "RB205_custom\3340_slow\data\U_slow_upper.paa",
+            "RB205_custom\3340_slow\data\U_slow_lower.paa",
+            "RB205_main\data\default\U_undersuit_co.paa"
+        };
+        LINKED_ITEMS("RB205_H_slow","RB205_V_slow","RB205_NV")
+        backpack = "RB205_B_radio";
+    };
+};
