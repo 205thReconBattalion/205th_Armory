19e04ac57b7b06edcafb2825ea9bb02bf4cbde2b
One <tim.one.gmc@gmail.com>
2024-04-22 23:25:38 +0200

    add: cfgammo hinzugefügt
    (model der tracer fehlt noch)

---
+++ addons/RB205_vehicles/CfgAmmo.hpp
@@ -0,0 +1,19 @@
+class cfgammo{
+
+    class 205_warthog_mg
+    {
+        model = "";
+        tracerStartTime = 0;
+        tracerEndTime = 10;
+        caliber = 3.4000001;
+        aiAmmoUsageFlags = "64 + 128 + 256";
+        cost = 15;
+        airFriction = -0.00036000001;
+        hit = 47.7;
+        indirectHit = 0;
+        indirectHitRange = 0;
+        typicalSpeed = 1215;
+        tracerScale = 1.3;
+    };
+
+}
