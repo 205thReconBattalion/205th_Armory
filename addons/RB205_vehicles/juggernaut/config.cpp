f8620009cbed5f2807f83142bd5e61354385d6e0
Spark2000 <paulalex.wetzel@gmail.com>
2024-09-06 19:47:03 +0200

    update: Juggernaut
    TODO: Lichter

--- addons/RB205_vehicles/juggernaut/config.cpp
+++ addons/RB205_vehicles/juggernaut/config.cpp
@@ -36,5 +36,50 @@ class cfgVehicles
 		faction = "RB205";
 		editorSubcategory = "RB205_veh_tank";
 		editorPreview = "";
+		hiddenSelectionsMaterials[] =
+		{
+			"RB205_vehicles\juggernaut\data\juggernaut_base.rvmat",
+			"RB205_vehicles\juggernaut\data\juggernaut_guns.rvmat",
+			"RB205_vehicles\juggernaut\data\juggernaut_props.rvmat",
+			"RB205_vehicles\juggernaut\data\juggernaut_wheels.rvmat"
+		};
+		hiddenSelectionsTextures[] =
+		{
+			"RB205_vehicles\juggernaut\data\juggernaut_base.paa",
+			"RB205_vehicles\juggernaut\data\juggernaut_guns.paa",
+			"RB205_vehicles\juggernaut\data\juggernaut_props.paa",
+			"RB205_vehicles\juggernaut\data\juggernaut_wheels.paa"
+		};
+		class textureSources
+		{
+			class Default
+			{
+				displayName = "205th Recon Battalion";
+				author = "205th Recon Battalion";
+				textures[] =
+				{
+					"RB205_vehicles\juggernaut\data\juggernaut_base.paa",
+					"RB205_vehicles\juggernaut\data\juggernaut_guns.paa",
+					"RB205_vehicles\juggernaut\data\juggernaut_props.paa",
+					"RB205_vehicles\juggernaut\data\juggernaut_wheels.paa"
+				};
+				factions[] =
+				{
+					"RB205"
+				};
+			};
+			class Republic
+			{
+				displayName = "Grand Army of the Republic";
+				author = "3AS";
+				textures[] =
+				{
+					"3as\3as_jug\data\juggernaut_base_co.paa",
+					"3as\3as_jug\data\juggernaut_guns_co.paa",
+					"3as\3as_jug\data\juggernaut_props_co.paa",
+					"3as\3as_jug\data\juggernaut_wheels_co.paa"
+				};
+			};
+		};
 	};
 };
