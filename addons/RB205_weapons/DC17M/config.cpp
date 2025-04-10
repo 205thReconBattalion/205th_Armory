84ce03e5dec7def4be9f42cb404d4b427ab890a7
Spark2000 <paulalex.wetzel@gmail.com>
2025-04-10 14:51:24 +0200

    fix: Fried Weapon Bug, Scope Bug
    Scope-Zoomstufen richtig eingestellt, Klassennamen wiederhergestellt, Genauigkeit für den Einzelschuss-Modus erhöht, Code-Redundanzen entfernt

--- addons/RB205_weapons/DC17M/config.cpp
+++ addons/RB205_weapons/DC17M/config.cpp
@@ -15,9 +15,9 @@ class cfgPatches
 		};
 		magazines[] =
 		{
-			"205_3AS_100Rnd_EC40_mag",
-			"205_3AS_5Rnd_EC80_mag",
-			"205_3AS_AntiArmour_mag"
+			"RB205_DC17M_Blaster_Mag",
+			"RB205_DC17M_Sniper_Mag",
+			"RB205_DC17M_AT_Mag"
 		};
 		ammo[] =
 		{
@@ -56,7 +56,7 @@ class CfgMagazines
 	class 3AS_5Rnd_EC80_mag;
 	class 3AS_AntiArmour_mag;
 
-	class 205_3AS_100Rnd_EC40_mag: 3AS_100Rnd_EC40_mag
+	class RB205_DC17M_Blaster_Mag: 3AS_100Rnd_EC40_mag
 	{
 		author = "205th Recon Battalion";
 		displayName = "$STR_205_DC17M_blaster_Energy_Pack_DisplayName";
@@ -66,7 +66,7 @@ class CfgMagazines
 		count = 60;
 		mass = 10;
 	};
-	class 205_3AS_5Rnd_EC80_mag: 3AS_5Rnd_EC80_mag
+	class RB205_DC17M_Sniper_Mag: 3AS_5Rnd_EC80_mag
 	{
 		author = "205th Recon Battalion";
 		displayName = "$STR_205_DC17M_sniper_Energy_Pack_DisplayName";
@@ -76,7 +76,7 @@ class CfgMagazines
 		count = 5;
 		mass = 4;
 	};
-	class 205_3AS_AntiArmour_mag: 3AS_AntiArmour_mag
+	class RB205_DC17M_AT_Mag: 3AS_AntiArmour_mag
 	{
 		author = "205th Recon Battalion";
 		displayName = "$STR_205_DC17M_at_Energy_Pack_DisplayName";
@@ -84,62 +84,20 @@ class CfgMagazines
 		descriptionShort = "$STR_205_DC17M_at_Energy_Pack_DescriptionShort";
 		ammo = "RB205_ammo_40mm_at";
 		count = 1;
-		mass = 15;//30;
+		mass = 15;
 	};
 };
 class CfgWeapons
 {
-	class SWLW_attachment_scope_DC17M_sniper;
-	class InventoryOpticsItem_Base_F;
-	class 3AS_DC17M_F;
-
-	class RB205_attachment_scope_DC17M_sniper: SWLW_attachment_scope_DC17M_sniper
+	class 3AS_DC17M_Base_F;
+	class 3AS_DC17M_F: 3AS_DC17M_Base_F
 	{
-		author = "205th Recon Battalion";
-		displayName = "$STR_205_OPTIC_DC17M_DisplayName";
-		class ItemInfo: InventoryOpticsItem_Base_F
+		class OpticsModes
 		{
-			mass = 8;
-			opticType = 1;
-			optics = 1;
-			modelOptics = "\SWLW_clones_spec\DC17M_attachment_sniper_optic.p3d";
-			class OpticsModes
-			{
-				class Scope
-				{
-					opticsID = 1;
-					useModelOptics = 1;
-					opticsPPEffects[]=
-					{
-						"OpticsCHAbera2",
-						"OpticsBlur3"
-					};
-					opticsZoomMin = 0.025;
-					opticsZoomMax = 0.125;
-					opticsZoomInit = 0.125;
-					discreteDistance[] = {100,300,400,500,600,700,800,900,1000};
-					discreteDistanceInitIndex = 1;
-					distanceZoomMin = 100;
-					distanceZoomMax = 1000;
-					discreteFov[] = {0.125,0.0625,0.025};
-					discreteInitIndex = 0;
-					memoryPointCamera = "opticView";
-					visionMode[]=
-					{
-						"Normal",
-						"NVG",
-						"TI"
-					};
-					opticsFlare = 1;
-					opticsDisablePeripherialVision = 1;
-					cameraDir = "";
-				};
-			};
+			class Ironsights;
+			class Scope;
 		};
-		inertia = 0.1;
 	};
-
-	FRIED_WEAPON(RB205_DC17M,"\RB205_weapons\data\ui\dc17m_fried.paa")
 	class RB205_DC17M: 3AS_DC17M_F
 	{
 		author = "205th Recon Battalion";
@@ -154,13 +112,11 @@ class CfgWeapons
 		};
 		magazines[] =
 		{
-			"205_3AS_100Rnd_EC40_mag",
-			"205_3AS_5Rnd_EC80_mag",
-			"205_3AS_AntiArmour_mag"
+			"RB205_DC17M_Blaster_Mag",
+			"RB205_DC17M_Sniper_Mag",
+			"RB205_DC17M_AT_Mag"
 		};
-		magazineWell[]=
-		{
-		};
+		magazineWell[] = {};
 		cursor = "RB205_CH_default";
 		JLTS_hasElectronics = 1;
 		JLTS_hasEMPProtection = 1;
@@ -169,65 +125,6 @@ class CfgWeapons
 		{
 			"Single",
 			"FullAuto"
-		};
-		maxZeroing=1000;
-		opticsZoomMin=0.005;
-		opticsZoomMax=1.25;
-		opticsZoomInit=0.75;
-		distanceZoomMin=40;
-		distanceZoomMax=4000;
-		
-		class OpticsModes
-		{
-			class Ironsights
-			{
-				opticsID=1;
-				useModelOptics=0;
-				opticsFlare="true";
-				opticsPPEffects[]=
-				{
-					"OpticsCHAbera5",
-					"OpticsBlur5"
-				};
-				opticsDisablePeripherialVision=0.67000002;
-				opticsZoomMin=0.25;
-				opticsZoomMax=1.25;
-				opticsZoomInit=0.75;
-				memoryPointCamera="eye";
-				visionMode[]={};
-				distanceZoomMin=100;
-				distanceZoomMax=100;
-			};
-			class Scope: Ironsights
-			{
-				opticsID=2;
-				useModelOptics=1;
-				opticsPPEffects[]=
-				{
-					"OpticsCHAbera5",
-					"OpticsBlur5"
-				};
-				opticsDisablePeripherialVision=0.67000002;
-				discreteDistance[] = {100,300,400,500,600,700,800,900,1000};
-				discreteDistanceInitIndex = 1;
-				distanceZoomMin = 100;
-				distanceZoomMax = 1000;
-				opticsZoomMin = 0.025;
-				opticsZoomMax = 0.125;
-				opticsZoomInit = 0.125;
-				discreteFov[] = {0.125,0.925,0.0625,0.325,0.025};
-				discreteInitIndex = 0;
-				memoryPointCamera="opticView";
-				visionMode[]=
-				{
-					"Normal",
-					"NVG",
-					"TI"
-				};
-				thermalMode[]={0};
-				opticsFlare="true";
-				cameraDir="";
-			};
 		};
 		class Single: Mode_SemiAuto
 		{
@@ -277,7 +174,7 @@ class CfgWeapons
 				};
 			};
 			reloadTime = 0.1;
-			dispersion = 0.0012;
+			dispersion = 0; //dispersion = 0.0012;
 			minRange = 5;
 			minRangeProbab = 0.30000001;
 			midRange = 25;
@@ -345,25 +242,19 @@ class CfgWeapons
 			aiRateOfFire = 2;
 			aiRateOfFireDistance = 25;
 		};
+		class OpticsModes: OpticsModes
+		{
+			class Ironsights: Ironsights {};
+			class Scope: Scope
+			{
+				discreteFov[] = {0.125,0.0625,0.025};
+				discreteInitIndex = 0;
+			};
+		};
 		class WeaponSlotsInfo: WeaponSlotsInfo
 		{
 			mass = 60;
-			class CowsSlot: CowsSlot
-			{
-				compatibleItems[] =
-				{
-					"RB205_attachment_scope_DC17M_sniper"
-				};
-			};
-			class PointerSlot: PointerSlot
-			{
-				linkProxy = "\A3\data_f\proxies\weapon_slots\SIDE";
-				compatibleItems[] =
-				{
-					"acc_flashlight",
-					"acc_pointer_IR"
-				};
-			};
 		};
 	};
+	FRIED_WEAPON(RB205_DC17M,"\RB205_weapons\data\ui\dc17m_fried.paa")
 };
