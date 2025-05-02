8481f3060b98aecc3d0e7936b86794b2da59cca0
Spark2000 <paulalex.wetzel@gmail.com>
2025-05-02 22:44:42 +0200

    update: DP-23

--- addons/RB205_weapons/DP23/config.cpp
+++ addons/RB205_weapons/DP23/config.cpp
@@ -5,7 +5,7 @@ class cfgPatches
 		requiredAddons[] =
 		{
 			"RB205_weapons",
-			"JLTS_weapons_DP23"
+			"3AS_Weapons_Republic_DP23"
 		};
 		requiredVersion = 1.0;
         author = "205th Recon Battalion";
@@ -19,104 +19,48 @@ class cfgPatches
 
 #include "\RB205_weapons\macros.hpp"
 
-class CfgWeapons
-{	
-	class WeaponSlotsInfo;
-	class CowsSlot;
-	class UnderBarrelSlot;
-	class MuzzleSlot;
-	class PointerSlot;
+class WeaponSlotsInfo;
+class CowsSlot;
+class PointerSlot;
+class MuzzleSlot;
 
-	class JLTS_DP23;
-	class RB205_DP23: JLTS_DP23
+class CfgWeapons
+{
+	class 3AS_DP23_F;
+	class RB205_DP23: 3AS_DP23_F
 	{
+		baseWeapon = "RB205_DP23";
+        author = "205th Recon Battalion";
 		displayName = "$STR_205_DP23_DisplayName";
 		displayNameShort = "$STR_205_DP23_DisplayNameShort";
-		descriptionShort = "Freigegeben für: 1-3 'Rancor'";
+		descriptionShort = "Streu-Blaster der GAR<br />Freigegeben für: Rancor (1-3)";
 		picture = "\3AS\3AS_Weapons\MPL\Data\UI\3as_mpl.paa";
+		cursor = "RB205_CH_shotgun";
 		mass = MASS_WP_CARBINE;
 		magazines[] =
 		{
 			"RB205_Scatter_Energy_Pack"
 		};
-		cursor = "RB205_CH_shotgun";
-		JLTS_canHaveShield=1;
-		JLTS_shieldedWeapon="RB205_DP23_shield";
+		magazineWell[] = {};
+		fireLightDiffuse[] = {0,0,1};
+		class WeaponSlotsInfo: WeaponSlotsInfo
+		{
+			class CowsSlot: CowsSlot
+			{
+				compatibleItems[] = {};
+			};
+			class PointerSlot: PointerSlot
+			{
+				compatibleItems[] = {};
+			};
+			class MuzzleSlot: MuzzleSlot
+			{
+				compatibleItems[] = {};
+			};
+		};
 		JLTS_hasElectronics = 1;
 		JLTS_hasEMPProtection = 0;
 		JLTS_friedItem = "RB205_DP23_fried";
-		baseWeapon = "RB205_DP23";
-        author = "205th Recon Battalion";
-		class WeaponSlotsInfo: WeaponSlotsInfo
-		{
-			mass=85;
-			class CowsSlot: CowsSlot
-			{
-				compatibleItems[]={};
-			};
-			class MuzzleSlot: MuzzleSlot
-			{
-				compatibleItems[]={};
-			};
-			class PointerSlot: PointerSlot
-			{
-				compatibleItems[]={};
-			};
-			class UnderBarrelSlot: UnderBarrelSlot
-			{
-				compatibleItems[]={};
-			};
-		};
 	};
 	FRIED_WEAPON(RB205_DP23,"\RB205_weapons\data\ui\dp23_fried.paa")
-
-	class JLTS_DP23_shield;
-	class RB205_DP23_shield: JLTS_DP23_shield
-	{
-		displayName = "$STR_205_DP23_DisplayName";
-		displayNameShort = "$STR_205_DP23_DisplayNameShort";
-		descriptionShort = "Freigegeben für: 1-3 'Rancor'";
-		picture = "\RB205_weapons\data\ui\dp23.paa";
-        author = "205th Recon Battalion";
-		magazines[] =
-		{
-			"RB205_Scatter_Energy_Pack"
-		};
-		JLTS_isShielded=1;
-		JLTS_baseWeapon="RB205_DP23";
-		baseWeapon = "RB205_DP23_shield";
-		JLTS_hasElectronics = 1;
-		JLTS_hasEMPProtection = 0;
-		JLTS_friedItem = "RB205_DP23_shield_fried";
-		class WeaponSlotsInfo: WeaponSlotsInfo
-		{
-			mass=110;
-			class UnderBarrelSlot: UnderBarrelSlot
-			{
-				compatibleItems[]=
-				{
-					"JLTS_riot_shield_attachment",
-					"JLTS_riot_shield_212_attachment",
-					"JLTS_riot_shield_501_attachment",
-					"JLTS_riot_shield_101_attachment",
-					"JLTS_riot_shield_CG_attachment",
-					"JLTS_riot_shield_GD_attachment",
-					"JLTS_riot_shield_droid_attachment"
-				};
-			};
-			class CowsSlot: CowsSlot
-			{
-				compatibleItems[]={};
-			};
-			class MuzzleSlot: MuzzleSlot
-			{
-				compatibleItems[]={};
-			};
-			class PointerSlot: PointerSlot
-			{
-				compatibleItems[]={};
-			};
-		};
-	};
-	FRIED_WEAPON(RB205_DP23_shield,"\RB205_weapons\data\ui\dp23_fried.paa")
 };
