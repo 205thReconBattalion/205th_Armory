df3ef91931015dab946d10bd928775fe75d8026a
Spark2000 <paulalex.wetzel@gmail.com>
2025-05-03 00:44:40 +0200

    update: DC-15S (Shielded)
    DC-15S (JLTS) entfernt, stattdessen Schild-Fkt. zu DC-15S hinzugefügt

--- addons/RB205_weapons/DC15S/config.cpp
+++ addons/RB205_weapons/DC15S/config.cpp
@@ -14,8 +14,7 @@ class cfgPatches
 		{
 			"RB205_DC15S",
 			"RB205_DC15S_GL",
-			"RB205_DC15S_JLTS",
-			"RB205_DC15S_JLTS_shield"
+			"RB205_DC15S_shield"
 		};
 	};
 };
@@ -26,9 +25,9 @@ class WeaponSlotsInfo;
 class CowsSlot;
 class PointerSlot;
 class MuzzleSlot;
+class UnderBarrelSlot;
 
 class Mode_SemiAuto;
-class Mode_FullAuto;
 
 class CfgWeapons
 {
@@ -94,6 +93,8 @@ class CfgWeapons
 				soundSetShot[] = { "RB205_DC15S_Shot_SoundSet" };
 			};
 		};
+		JLTS_canHaveShield = 1;
+		JLTS_shieldedWeapon = "RB205_DC15S_shield";
 		JLTS_hasElectronics = 1;
 		JLTS_hasEMPProtection = 0;
 		JLTS_friedItem = "RB205_DC15S_fried";
@@ -200,163 +201,39 @@ class CfgWeapons
 	};
 	FRIED_WEAPON(RB205_DC15S_GL,"\RB205_weapons\data\ui\dc15s_fried.paa")
 
-
-
-	class Rifle_Base_F;
-	class arifle_MX_Base_F: Rifle_Base_F
+	class RB205_DC15S_shield: RB205_DC15S
 	{
-		class WeaponSlotsInfo;
-	};
-	class JLTS_DC15S: arifle_MX_Base_F
-	{
+		scope = 1;
+		baseWeapon = "RB205_DC15S_shield";
+		displayName = "$STR_205_DC15S_shield_DisplayName";
+		model = "\MRC\JLTS\weapons\DC15S\DC15S_shielded.p3d";
+		hiddenSelections[] = {"camo1","camo2"};
+		hiddenSelectionsTextures[] = {"\MRC\JLTS\weapons\DC15S\data\DC15S_co.paa","\MRC\JLTS\weapons\Shield\data\shield_co.paa"};
+		handAnim[] = {"OFP2_ManSkeleton","\MRC\JLTS\weapons\DC15S\anims\DC15S_shielded_handanim.rtm"};
+		reloadAction = "GestureReload";
 		class WeaponSlotsInfo: WeaponSlotsInfo
 		{
-			class CowsSlot;
-		};
-		class Single: Mode_SemiAuto
-		{
-			class StandardSound;
-		};
-		class FullAuto: Mode_FullAuto
-		{
-			class StandardSound;
-		};
-	};
-	class RB205_DC15S_JLTS: JLTS_DC15S
-	{
-		baseWeapon = "RB205_DC15S_JLTS";
-		author = "205th Recon Battalion";
-		displayName = "$STR_205_DC15S_JLTS_DisplayName";
-		displayNameShort = "$STR_205_DC15S_DisplayNameShort";
-		descriptionShort = "Standard-Blasterkarabiner der GAR<br />Freigegeben für: Alle";
-		picture = "\3AS\3AS_Weapons\Republic\DC15S\Data\UI\3as_dc15s.paa";
-		mass = MASS_WP_CARBINE;
-		fireLightDiffuse[] = {0,0,1};
-		magazines[] =
-		{
-			"RB205_Standard_Energy_Pack"
-		};
-		cursor = "RB205_CH_default";
-		magazineWell[] = {};
-		muzzles[] = {"this"};
-		JLTS_hasElectronics = 1;
-		JLTS_hasEMPProtection = 0;
-		JLTS_friedItem = "RB205_DC15S_JLTS_fried";
-		JLTS_canHaveShield = 1;
-		JLTS_shieldedWeapon = "RB205_DC15S_JLTS_shield";
-		opticsZoomInit = 0.75;
-		opticsZoomMax = 1.25;
-		opticsZoomMin = 0.25;
-		class LinkedItems
-		{
-			item = "3AS_optic_holo_DC15S";
-			slot = "CowsSlot";
-		};
-		class WeaponSlotsInfo: WeaponSlotsInfo
-		{
-			class CowsSlot: CowsSlot
+			class UnderBarrelSlot: UnderBarrelSlot
 			{
-				compatibleItems[] = {};
-				iconScale = 0;
-				iconPosition[] = {0,0};
-				iconPicture = "";
+				compatibleItems[] =
+				{
+					"JLTS_riot_shield_attachment",
+					"JLTS_riot_shield_101_attachment",
+					"JLTS_riot_shield_212_attachment",
+					"JLTS_riot_shield_501_attachment",
+					"JLTS_riot_shield_CG_attachment",
+					"JLTS_riot_shield_GD_attachment",
+					"JLTS_riot_shield_droid_attachment"
+				};
 			};
 		};
-		class Single: Single
-		{
-			dispersion = 0.0012;
-			class StandardSound//: StandardSound
-			{
-				soundSetShot[] = { "RB205_DC15S_Shot_SoundSet" };
-			};
-			class SilencedSound {};
-		};
-		class FullAuto: FullAuto
-		{
-			dispersion = 0.0012;
-			class StandardSound//: StandardSound
-			{
-				soundSetShot[] = { "RB205_DC15S_Shot_SoundSet" };
-			};
-			class SilencedSound {};
-		};
-	};
-	FRIED_WEAPON(RB205_DC15S_JLTS,"\RB205_weapons\data\ui\dc15s_fried.paa")
-
-	class JLTS_DC15S_shield: JLTS_DC15S
-	{
-		class WeaponSlotsInfo: WeaponSlotsInfo
-		{
-			class CowsSlot;
-		};
-		class Single: Mode_SemiAuto
-		{
-			class StandardSound;
-		};
-		class FullAuto: Mode_FullAuto
-		{
-			class StandardSound;
-		};
-	};
-	class RB205_DC15S_JLTS_shield: JLTS_DC15S_shield
-	{
-		baseWeapon = "RB205_DC15S_JLTS_shield";
-		author = "205th Recon Battalion";
-		displayName = "$STR_205_DC15S_JLTS_shield_DisplayName";
-		displayNameShort = "$STR_205_DC15S_DisplayNameShort";
-		descriptionShort = "Standard-Blasterkarabiner der GAR<br />Freigegeben für: Alle";
-		picture = "\3AS\3AS_Weapons\Republic\DC15S\Data\UI\3as_dc15s.paa";
-		fireLightDiffuse[] = {0,0,1};
-		magazines[] =
-		{
-			"RB205_Standard_Energy_Pack"
-		};
-		cursor = "RB205_CH_default";
-		magazineWell[] = {};
-		muzzles[] = {"this"};
-		JLTS_hasElectronics = 1;
-		JLTS_hasEMPProtection = 0;
-		JLTS_friedItem = "RB205_DC15S_JLTS_shield_fried";
 		JLTS_isShielded = 1;
-		JLTS_baseWeapon = "RB205_DC15S_JLTS";
-		opticsZoomInit = 0.75;
-		opticsZoomMax = 1.25;
-		opticsZoomMin = 0.25;
-		class LinkedItems
-		{
-			item = "3AS_optic_holo_DC15S";
-			slot = "CowsSlot";
-		};
-		class WeaponSlotsInfo: WeaponSlotsInfo
-		{
-			class CowsSlot: CowsSlot
-			{
-				compatibleItems[] = { "3AS_optic_holo_DC15S" };
-				iconScale = 0;
-				iconPosition[] = {0,0};
-				iconPicture = "";
-			};
-		};
-		class Single: Single
-		{
-			dispersion = 0.0012;
-			class StandardSound: StandardSound
-			{
-				soundSetShot[] = { "RB205_DC15S_Shot_SoundSet" };
-			};
-			class SilencedSound {};
-		};
-		class FullAuto: FullAuto
-		{
-			dispersion = 0.0012;
-			class StandardSound: StandardSound
-			{
-				soundSetShot[] = { "RB205_DC15S_Shot_SoundSet" };
-			};
-			class SilencedSound {};
-		};
+		JLTS_baseWeapon = "RB205_DC15S";
+		JLTS_hasElectronics = 1;
+		JLTS_hasEMPProtection = 0;
+		JLTS_friedItem = "RB205_DC15S_shield_fried";
 	};
-	FRIED_WEAPON(RB205_DC15S_JLTS_shield,"\RB205_weapons\data\ui\dc15s_fried.paa")
+	FRIED_WEAPON(RB205_DC15S_shield,"\RB205_weapons\data\ui\dc15s_fried.paa")
 };
 
 class cfgSoundShaders
