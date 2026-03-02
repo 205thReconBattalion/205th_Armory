71902139b6cec5c90906814dc6e91a17b9cde5e5
Spark <paulalex.wetzel@gmail.com>
2026-03-02 23:26:42 +0100

    update: binoculars

--- addons/RB205_weapons/binoculars/config.cpp
+++ addons/RB205_weapons/binoculars/config.cpp
@@ -26,14 +26,14 @@ class CfgWeapons
 		displayNameShort = "$STR_205_BINO_M_DisplayNameShort";
 		descriptionShort = "Freigegeben für: Alle";
         author = "205th Recon Battalion";
-		picture = "\RB205_weapons\data\ui\binocular.paa";
+		picture = "\RB205_weapons\binoculars\data\ui_binocular.paa";
 		hiddenSelectionsMaterials[] =
 		{
-			"\RB205_weapons\data\materials\binocular.rvmat"
+			"\RB205_weapons\binoculars\data\binocular.rvmat"
 		};
 		hiddenSelectionsTextures[] =
 		{
-			"\RB205_weapons\data\RB205_binocular_co.paa"
+			"\RB205_weapons\binoculars\data\RB205_binocular_co.paa"
 		};
 		visionMode[] = { "Normal", "NVG" };
 	};
@@ -45,14 +45,14 @@ class CfgWeapons
 		displayNameShort = "$STR_205_BINO_E_DisplayNameShort";
 		descriptionShort = "Freigegeben für: ARF, CL+";
         author = "205th Recon Battalion";
-		picture = "\RB205_weapons\data\ui\binocular_arf.paa";
+		picture = "\RB205_weapons\binoculars\data\ui_binocular_arf.paa";
 		hiddenSelectionsMaterials[] =
 		{
-			"\RB205_weapons\data\materials\binocular_arf.rvmat"
+			"\RB205_weapons\binoculars\data\binocular_arf.rvmat"
 		};
 		hiddenSelectionsTextures[] =
 		{
-			"\RB205_weapons\data\RB205_binocular_arf_blk_co.paa"
+			"\RB205_weapons\binoculars\data\RB205_binocular_arf_blk_co.paa"
 		};
 		visionMode[] = { "Normal", "NVG", "TI" };
 	    thermalMode[] = {0};
