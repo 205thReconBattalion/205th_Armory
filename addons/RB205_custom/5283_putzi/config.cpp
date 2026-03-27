10bd6c1d1f847d36c977a5a5f95f4ce6624ee802
Spark <paulalex.wetzel@gmail.com>
2026-03-28 00:02:04 +0100

    update: Putzi

--- addons/RB205_custom/5283_putzi/config.cpp
+++ addons/RB205_custom/5283_putzi/config.cpp
@@ -25,50 +25,50 @@ class cfgPatches
 
 class cfgWeapons
 {
-    class RB205_H_ab_trooper;
-    class RB205_H_putzi : RB205_H_ab_trooper
+    class RB205_H_arc_trooper;
+    class RB205_H_putzi : RB205_H_arc_trooper
     {
-        displayName = "[205] Clone Airborne Trooper Helmet [5283]";
+        displayName = "[205] Clone ARC Trooper Helmet [5283]";
         hiddenSelectionsTextures[] =
         {
             "RB205_custom\5283_putzi\data\H_putzi.paa",
-            "RB205_main\data\airborne\visor_ab_co.paa"
+            "RB205_main\data\arc\visor_arc_co.paa",
+            "RB205_custom\5283_putzi\data\H_putzi.paa"
         };
     };
 
     class RB205_U_sgt_base;
-    class RB205_U_sergeant: RB205_U_sgt_base
+    class RB205_U_arc_trooper: RB205_U_sgt_base
     {
         class ItemInfo;
     };
-    class RB205_U_putzi : RB205_U_sergeant
+    class RB205_U_putzi : RB205_U_arc_trooper
     {
-        displayName = "[205] Clone Trooper Armor [5283]";
+        displayName = "[205] Clone ARC Trooper Armor [5283]";
         class ItemInfo : ItemInfo
         {
             uniformClass = RB205_clone_putzi;
         };
     };
 
-    class RB205_V_ab_cs;
-    class RB205_V_putzi: RB205_V_ab_cs
+    class RB205_V_arc;
+    class RB205_V_putzi: RB205_V_arc
     {
-        displayName = "[205] Clone Trooper Vest [5283]";
+        displayName = "[205] Clone ARC Trooper Vest [5283]";
         hiddenSelectionsTextures[] =
         {
-            "RB205_custom\5283_putzi\data\V_putzi_acc.paa",
+            "RB205_custom\5283_putzi\data\V_arc_putzi.paa",
             "RB205_custom\5283_putzi\data\V_putzi.paa",
-            "RB205_custom\5283_putzi\data\V_putzi_acc.paa"
         };
     };
 };
 
 class cfgVehicles
 {
-    class RB205_clone_ab_trooper;
-    class RB205_clone_putzi : RB205_clone_ab_trooper
+    class RB205_clone_arc_trooper;
+    class RB205_clone_putzi : RB205_clone_arc_trooper
     {
-        displayName = "CS-5283 Putzi";
+        displayName = "ARC-5283 Putzi";
         uniformclass = "RB205_U_putzi";
         editorSubCategory = "RB205_lore";
         hiddenselectionsTextures[] =
@@ -77,6 +77,6 @@ class cfgVehicles
             "RB205_custom\5283_putzi\data\U_putzi_lower.paa",
             "RB205_main\data\default\U_undersuit_co.paa"
         };
-        LINKED_ITEMS("RB205_H_putzi", "RB205_V_putzi", "RB205_NV_chip")
+        LINKED_ITEMS("RB205_H_putzi", "RB205_V_putzi", "RB205_NV_rangefinder")
     };
 };
