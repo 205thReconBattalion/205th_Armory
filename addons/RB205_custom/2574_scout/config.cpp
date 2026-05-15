c9424da408d37caf7ae0d26e9ef485e5b9ec66a4
Spark <paulalex.wetzel@gmail.com>
2026-05-15 22:08:57 +0200

    update: Scout

--- addons/RB205_custom/2574_scout/config.cpp
+++ addons/RB205_custom/2574_scout/config.cpp
@@ -24,15 +24,14 @@ class cfgPatches
 
 class cfgWeapons
 {
-    class RB205_H_arf_trooper;
-    class RB205_H_scout : RB205_H_arf_trooper
+    class RB205_H_trooper;
+    class RB205_H_scout : RB205_H_trooper
     {
         displayName = "[205] Clone Trooper Helmet [2574]";
         hiddenSelectionsTextures[] =
         {
             "RB205_custom\2574_scout\data\H_scout.paa",
-            "",
-            "RB205_main\data\arf\visor_arf_co.paa"
+            "RB205_main\data\default\visor_p2_co.paa"
         };
     };
 
@@ -41,10 +40,10 @@ class cfgWeapons
     {
         class ItemInfo;
     };
-    class RB205_U_scout : RB205_U_trooper
+    class RB205_U_scout: RB205_U_trooper
     {
         displayName = "[205] Clone Trooper Armor [2574]";
-        class ItemInfo : ItemInfo
+        class ItemInfo: ItemInfo
         {
             uniformClass = RB205_clone_scout;
         };
@@ -54,17 +53,17 @@ class cfgWeapons
 class cfgVehicles
 {
     class RB205_clone_trooper;
-    class RB205_clone_scout : RB205_clone_trooper
+    class RB205_clone_scout: RB205_clone_trooper
     {
         displayName = "CT-2574 Scout";
-        uniformclass = "RB205_U_scout";
+        uniformClass = "RB205_U_scout";
         editorSubCategory = "RB205_lore";
-        hiddenselectionsTextures[] =
+        hiddenSelectionsTextures[] =
         {
             "RB205_custom\2574_scout\data\U_scout_upper.paa",
             "RB205_custom\2574_scout\data\U_scout_lower.paa",
             "RB205_main\data\default\U_undersuit_co.paa"
         };
-        LINKED_ITEMS("RB205_H_scout", "RB205_V_arf_ct", "RB205_NV_arf_antenna")
+        LINKED_ITEMS("RB205_H_scout", "RB205_V_ct", "RB205_NV_chip")
     };
 };
