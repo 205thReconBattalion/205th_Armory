93002279ed2f9f87b559183cdb1032dc85e18359
Potzga <siegfried-71@gmx.de>
2026-06-24 22:04:40 +0200

    DialogMitButtonBildern

---
+++ addons/RB205_roleDialog/UIElements/defines.hpp
@@ -0,0 +1,111 @@
+#define CT_SHORTCUTBUTTON   16
+#define ST_CENTER           2
+
+#define HINTERGRUND_X       0.35 * safezoneW + safezoneX
+#define HINTERGRUND_Y       0.20 * safezoneH + safezoneY
+#define HINTERGRUND_W       0.30 * safezoneW
+#define HINTERGRUND_H       0.60 * safezoneH
+
+#define BUTTON_W            0.05 * safezoneW
+#define BUTTON_H            0.075 * safezoneH
+
+#define SPALTE_EINS         HINTERGRUND_X + 0.01 * safezoneW
+#define SPALTE_ZWEI         HINTERGRUND_X + 0.08 * safezoneW
+#define SPALTE_DREI         HINTERGRUND_X + 0.15 * safezoneW
+#define SPALTE_VIER         HINTERGRUND_X + 0.22 * safezoneW
+
+#define ZEILE_EINS          HINTERGRUND_Y + 0.05 * safezoneH
+#define ZEILE_ZWEI          HINTERGRUND_Y + 0.16 * safezoneH
+#define ZEILE_DREI          HINTERGRUND_Y + 0.27 * safezoneH
+
+
+
+
+
+class RB205RoleDialog_RscButton
+{
+    idc = -1;
+    type = CT_SHORTCUTBUTTON;
+    style = 0;
+
+    text = "";
+    action = "";
+
+    color[] = {1,1,1,1};
+    color2[] = {1,1,1,1};
+    colorDisabled[] = {0.5,0.5,0.5,0.5};
+    colorFocused[] = {0,0,0,0};
+
+    colorBackground[] = {1,1,1,0.85};
+    colorBackground2[] = {0.55,0.08,0.08,1};
+    colorBackgroundFocused[] = {0,0,0,0};
+
+    autoAdjustTexture = true;
+
+    textureNoShortcut = "";
+
+    onMouseEnter = "_this select 0 ctrlSetBackgroundColor  [0.4157,0.0588,0.0588,0.85]; (_this select 0) ctrlCommit 0.1;";
+    onMouseExit = "_this select 0 ctrlSetBackgroundColor  [1,1,1,0.85]; (_this select 0) ctrlCommit 0.1;";
+
+    class HitZone
+    {
+        left = 0;
+        top = 0;
+        right = 0;
+        bottom = 0;
+    };
+
+    class ShortcutPos
+    {
+        left = 0;
+        top = 0;
+        w = 1;
+        h = 1;
+    };
+
+    class TextPos
+    {
+        left = 0;
+        top = 0;
+        right = 0;
+        bottom = 0;
+    };
+
+    period = 0;
+    periodFocus = 0;
+    periodOver = 0;
+
+    font = "PuristaMedium";
+    size = 0;
+    sizeEx = 0;
+
+    soundEnter[] = {"",0.09,1};
+    soundPush[] = {"",0.09,1};
+    soundClick[] = {"",0.09,1};
+    soundEscape[] = {"",0.09,1};
+};
+
+
+
+class RB205RoleDialog_RscPicture
+{
+    access = 0;
+    type = 0;
+    idc = -1;
+
+    style = 48;
+
+    font = "PuristaMedium";
+    sizeEx = 0;
+
+    colorBackground[] = {0,0,0,0};
+    colorText[] = {1,1,1,1};
+
+    lineSpacing = 0;
+    fixedWidth = 0;
+
+    shadow = 0;
+
+    text = "";
+};
+
---
+++ addons/RB205_roleDialog/UIElements/dialog.hpp
@@ -0,0 +1,255 @@
+class RB205_roleDialog
+{
+    idd = 205530;
+    movingEnable = 0;
+    enableSimulation = 1;
+
+    class controlsBackground
+    {
+
+        class DialogBackground: RB205RoleDialog_RscPicture
+        {
+            idc = -1;
+            x = HINTERGRUND_X;
+            y = HINTERGRUND_Y;
+            w = HINTERGRUND_W;
+            h = HINTERGRUND_H;
+            colorBackground[] = {0,0,0,0.8};
+            text = "RB205_roleDialog\UIElements\bilder\HintergrundDialog.paa";
+        };
+    };
+
+    class controls
+    {
+        class ButtonRifleman: RB205RoleDialog_RscButton
+        {
+            idc = 205531;
+            text = "";
+            x = SPALTE_EINS;
+            y = ZEILE_EINS;
+            w = 0.05 * safezoneW;
+            h = 0.075 * safezoneH;
+
+            
+            animTextureNormal = "\RB205_roleDialog\UIElements\bilder\Rifleman.paa";
+            animTextureDisabled = "\RB205_roleDialog\UIElements\bilder\Rifleman.paa";
+            animTextureOver = "\RB205_roleDialog\UIElements\bilder\Rifleman.paa";
+            animTextureFocused = "\RB205_roleDialog\UIElements\bilder\Rifleman.paa";
+            animTexturePressed = "\RB205_roleDialog\UIElements\bilder\Rifleman.paa";
+            animTextureDefault = "\RB205_roleDialog\UIElements\bilder\Rifleman.paa";
+
+            onButtonClick = "call RB205_roleDialog_fnc_onBtnClickRifleman";
+        };
+        
+        class ButtonATAA: RB205RoleDialog_RscButton
+        {
+            idc = 205532;
+            text = "";
+            x = SPALTE_EINS;
+            y = ZEILE_ZWEI;
+            w = 0.05 * safezoneW;
+            h = 0.075 * safezoneH;
+            
+            animTextureNormal = "\RB205_roleDialog\UIElements\bilder\AT_AA.paa";
+            animTextureDisabled = "\RB205_roleDialog\UIElements\bilder\AT_AA.paa";
+            animTextureOver = "\RB205_roleDialog\UIElements\bilder\AT_AA.paa";
+            animTextureFocused = "\RB205_roleDialog\UIElements\bilder\AT_AA.paa";
+            animTexturePressed = "\RB205_roleDialog\UIElements\bilder\AT_AA.paa";
+            animTextureDefault = "\RB205_roleDialog\UIElements\bilder\AT_AA.paa";
+
+            onButtonClick = "call RB205_roleDialog_fnc_onBtnClickATAA";
+        };
+        class ButtonEOD: RB205RoleDialog_RscButton
+        {
+            idc = 205533;
+            text = "";
+            x = SPALTE_EINS;
+            y = ZEILE_DREI;
+            w = 0.05 * safezoneW;
+            h = 0.075 * safezoneH;
+            
+            animTextureNormal = "\RB205_roleDialog\UIElements\bilder\EOD.paa";
+            animTextureDisabled = "\RB205_roleDialog\UIElements\bilder\EOD.paa";
+            animTextureOver = "\RB205_roleDialog\UIElements\bilder\EOD.paa";
+            animTextureFocused = "\RB205_roleDialog\UIElements\bilder\EOD.paa";
+            animTexturePressed = "\RB205_roleDialog\UIElements\bilder\EOD.paa";
+            animTextureDefault = "\RB205_roleDialog\UIElements\bilder\EOD.paa";
+
+            onButtonClick = "call RB205_roleDialog_fnc_onBtnClickEOD";
+        };
+
+        class ButtonARF: RB205RoleDialog_RscButton
+        {
+            idc = 205540;
+            text = "";
+            x = SPALTE_ZWEI;
+            y = ZEILE_EINS;
+            w = 0.05 * safezoneW;
+            h = 0.075 * safezoneH;
+            
+            animTextureNormal = "\RB205_roleDialog\UIElements\bilder\ARF.paa";
+            animTextureDisabled = "\RB205_roleDialog\UIElements\bilder\ARF.paa";
+            animTextureOver = "\RB205_roleDialog\UIElements\bilder\ARF.paa";
+            animTextureFocused = "\RB205_roleDialog\UIElements\bilder\ARF.paa";
+            animTexturePressed = "\RB205_roleDialog\UIElements\bilder\ARF.paa";
+            animTextureDefault = "\RB205_roleDialog\UIElements\bilder\ARF.paa";
+
+            onButtonClick = "call RB205_roleDialog_fnc_onBtnClickARF";
+        };
+        
+        class ButtonGren: RB205RoleDialog_RscButton
+        {
+            idc = 205534;
+            text = "";
+            x = SPALTE_ZWEI;
+            y = ZEILE_ZWEI;
+            w = 0.05 * safezoneW;
+            h = 0.075 * safezoneH;
+            
+            animTextureNormal = "\RB205_roleDialog\UIElements\bilder\Gren.paa";
+            animTextureDisabled = "\RB205_roleDialog\UIElements\bilder\Gren.paa";
+            animTextureOver = "\RB205_roleDialog\UIElements\bilder\Gren.paa";
+            animTextureFocused = "\RB205_roleDialog\UIElements\bilder\Gren.paa";
+            animTexturePressed = "\RB205_roleDialog\UIElements\bilder\Gren.paa";
+            animTextureDefault = "\RB205_roleDialog\UIElements\bilder\Gren.paa";
+
+            onButtonClick = "call RB205_roleDialog_fnc_onBtnClickGren";
+        };
+
+        class ButtonHeavy: RB205RoleDialog_RscButton
+        {
+            idc = 205535;
+            text = "";
+            x = SPALTE_ZWEI;
+            y = ZEILE_DREI;
+            w = 0.05 * safezoneW;
+            h = 0.075 * safezoneH;
+            
+            animTextureNormal = "\RB205_roleDialog\UIElements\bilder\Heavy.paa";
+            animTextureDisabled = "\RB205_roleDialog\UIElements\bilder\Heavy.paa";
+            animTextureOver = "\RB205_roleDialog\UIElements\bilder\Heavy.paa";
+            animTextureFocused = "\RB205_roleDialog\UIElements\bilder\Heavy.paa";
+            animTexturePressed = "\RB205_roleDialog\UIElements\bilder\Heavy.paa";
+            animTextureDefault = "\RB205_roleDialog\UIElements\bilder\Heavy.paa";
+
+            onButtonClick = "call RB205_roleDialog_fnc_onBtnClickHeavy";
+        };
+
+        class ButtonMarksman: RB205RoleDialog_RscButton
+        {
+            idc = 205536;
+            text = "";
+            x = SPALTE_DREI;
+            y = ZEILE_EINS;
+            w = 0.05 * safezoneW;
+            h = 0.075 * safezoneH;
+            
+            animTextureNormal = "\RB205_roleDialog\UIElements\bilder\Marksman.paa";
+            animTextureDisabled = "\RB205_roleDialog\UIElements\bilder\Marksman.paa";
+            animTextureOver = "\RB205_roleDialog\UIElements\bilder\Marksman.paa";
+            animTextureFocused = "\RB205_roleDialog\UIElements\bilder\Marksman.paa";
+            animTexturePressed = "\RB205_roleDialog\UIElements\bilder\Marksman.paa";
+            animTextureDefault = "\RB205_roleDialog\UIElements\bilder\Marksman.paa";
+
+            onButtonClick = "call RB205_roleDialog_fnc_onBtnClickMarksman";
+        };
+
+        class ButtonCrewman: RB205RoleDialog_RscButton
+        {
+            idc = 205541;
+            text = "";
+            x = SPALTE_DREI;
+            y = ZEILE_ZWEI;
+            w = 0.05 * safezoneW;
+            h = 0.075 * safezoneH;
+            
+            animTextureNormal = "\RB205_roleDialog\UIElements\bilder\Crewman.paa";
+            animTextureDisabled = "\RB205_roleDialog\UIElements\bilder\Crewman.paa";
+            animTextureOver = "\RB205_roleDialog\UIElements\bilder\Crewman.paa";
+            animTextureFocused = "\RB205_roleDialog\UIElements\bilder\Crewman.paa";
+            animTexturePressed = "\RB205_roleDialog\UIElements\bilder\Crewman.paa";
+            animTextureDefault = "\RB205_roleDialog\UIElements\bilder\Crewman.paa";
+
+            onButtonClick = "call RB205_roleDialog_fnc_onBtnClickCrewman";
+        };
+
+        class ButtonMedic: RB205RoleDialog_RscButton
+        {
+            idc = 205537;
+            text = "";
+            x = SPALTE_DREI;
+            y = ZEILE_DREI;
+            w = 0.05 * safezoneW;
+            h = 0.075 * safezoneH;
+            
+            animTextureNormal = "\RB205_roleDialog\UIElements\bilder\Medic.paa";
+            animTextureDisabled = "\RB205_roleDialog\UIElements\bilder\Medic.paa";
+            animTextureOver = "\RB205_roleDialog\UIElements\bilder\Medic.paa";
+            animTextureFocused = "\RB205_roleDialog\UIElements\bilder\Medic.paa";
+            animTexturePressed = "\RB205_roleDialog\UIElements\bilder\Medic.paa";
+            animTextureDefault = "\RB205_roleDialog\UIElements\bilder\Medic.paa";
+
+            onButtonClick = "call RB205_roleDialog_fnc_onBtnClickMedic";
+        };
+
+        class ButtonRTO: RB205RoleDialog_RscButton
+        {
+            idc = 205538;
+            text = "";
+            x = SPALTE_VIER;
+            y = ZEILE_EINS;
+            w = 0.05 * safezoneW;
+            h = 0.075 * safezoneH;
+            
+            animTextureNormal = "\RB205_roleDialog\UIElements\bilder\RTO.paa";
+            animTextureDisabled = "\RB205_roleDialog\UIElements\bilder\RTO.paa";
+            animTextureOver = "\RB205_roleDialog\UIElements\bilder\RTO.paa";
+            animTextureFocused = "\RB205_roleDialog\UIElements\bilder\RTO.paa";
+            animTexturePressed = "\RB205_roleDialog\UIElements\bilder\RTO.paa";
+            animTextureDefault = "\RB205_roleDialog\UIElements\bilder\RTO.paa";
+
+            onButtonClick = "call RB205_roleDialog_fnc_onBtnClickRTO";
+        };
+
+        class ButtonPilot: RB205RoleDialog_RscButton
+        {
+            idc = 205539;
+            text = "";
+            x = SPALTE_VIER;
+            y = ZEILE_ZWEI;
+            w = 0.05 * safezoneW;
+            h = 0.075 * safezoneH;
+            
+            animTextureNormal = "\RB205_roleDialog\UIElements\bilder\Pilot.paa";
+            animTextureDisabled = "\RB205_roleDialog\UIElements\bilder\Pilot.paa";
+            animTextureOver = "\RB205_roleDialog\UIElements\bilder\Pilot.paa";
+            animTextureFocused = "\RB205_roleDialog\UIElements\bilder\Pilot.paa";
+            animTexturePressed = "\RB205_roleDialog\UIElements\bilder\Pilot.paa";
+            animTextureDefault = "\RB205_roleDialog\UIElements\bilder\Pilot.paa";
+
+            onButtonClick = "call RB205_roleDialog_fnc_onBtnClickPilot";
+        };
+
+        class ButtonARC: RB205RoleDialog_RscButton
+        {
+            idc = 205542;
+            text = "";
+            x = SPALTE_VIER;
+            y = ZEILE_DREI;
+            w = 0.05 * safezoneW;
+            h = 0.075 * safezoneH;
+            
+            animTextureNormal = "\RB205_roleDialog\UIElements\bilder\ARC.paa";
+            animTextureDisabled = "\RB205_roleDialog\UIElements\bilder\ARC.paa";
+            animTextureOver = "\RB205_roleDialog\UIElements\bilder\ARC.paa";
+            animTextureFocused = "\RB205_roleDialog\UIElements\bilder\ARC.paa";
+            animTexturePressed = "\RB205_roleDialog\UIElements\bilder\ARC.paa";
+            animTextureDefault = "\RB205_roleDialog\UIElements\bilder\ARC.paa";
+
+            onButtonClick = "call RB205_roleDialog_fnc_onBtnClickARC";
+        };
+
+
+
+    };
+};
