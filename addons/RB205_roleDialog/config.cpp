0884b9024a110fbf6069ad3a38a897140f25886f
Potzga <siegfried-71@gmx.de>
2026-06-24 22:03:25 +0200

    configFunktionenHinzugefuegt

---
+++ addons/RB205_roleDialog/config.cpp
@@ -0,0 +1,47 @@
+class CfgPatches
+{
+	class RB205_roleDialog
+	{
+		requiredAddons[] = {};
+		requiredVersion = 1.0;
+		author = "Putzi";
+		units[] = {};
+		weapons[] = {};
+	};
+};
+class cfgFunctions
+{
+    class RB205_roleDialog
+    {
+        class onBtnClick
+        {
+            file = "RB205_roleDialog\functions";
+            class onBtnClickARC {};
+            class onBtnClickARF {};
+            class onBtnClickATAA {};
+            class onBtnClickCrewman {};
+            class onBtnClickEOD {};
+            class onBtnClickGren {};
+            class onBtnClickHeavy {};
+            class onBtnClickMarksman {};
+            class onBtnClickMedic {};
+            class onBtnClickPilot {};
+            class onBtnClickRifleman {};
+            class onBtnClickRTO {};
+            class permissionARC {};
+            class permissionCrewman {};
+            class permissionEOD {};
+            class permissionMedic {};
+            class permissionPilot {};
+            class permissionRifleman {};
+        };
+    };
+};
+
+
+
+
+
+
+#include "UIElements\defines.hpp"
+#include "UIElements\dialog.hpp"
